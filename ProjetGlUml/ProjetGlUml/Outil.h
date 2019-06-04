5ae2e315f75f96256c1fd66ec74cffbd342840c5
FatimaW <50332888+FatimaW@users.noreply.github.com>
2019-06-04 12:09:24 +0200

    updated

--- ProjetGlUml/ProjetGlUml/Outil.h
+++ ProjetGlUml/ProjetGlUml/Outil.h
@@ -101,12 +101,13 @@ protected:
 	//La méthode retourne la map qui à chaque type de gaz associe les moyennes
 	//des mesures effectuées dans le contexte.
 
-	multimap<Capteur, Capteur> chercherCaptSimilaires(const Contexte * contexte);
+	multimap<pair<Capteur, Capteur>,string> chercherCaptSimilaires(const Contexte * contexte);
 	//Mode d'emploi :
 	//méthode permettant de trouver les capteurs ayant des comportements similaires
 	//se trouvant dans la zone définie par le contexte passé en paramètre.
 	//Elle retourne la multimap contenant les paires de capteurs ayant envoyé 
-	//des données similaires lors de leurs dix dernieres mesures
+	//des données similaires dans l'ensemble, associées aux types
+	//de mesure correspondants.
 
 	map <string, double> trouverValeursCaract(const Contexte * contexte);
 	//Mode d'emploi :
